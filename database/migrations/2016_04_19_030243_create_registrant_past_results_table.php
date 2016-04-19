95c0becb979c5ab357eadb8279fd821bd18cfd34
Jimmy Gleason <jimmy.gleason@akqa.com>
2016-04-19 00:13:45 -0400

    registrations, registrant past results, and configuration migrations

---
+++ database/migrations/2016_04_19_030243_create_registrant_past_results_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrantPastResultsTable extends Migration
+{
+    /**
+     * Create registrants past results table.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrant_past_results', function (Blueprint $table) {
+
+            $table->increments('id')->unsigned();
+            $table->integer('registration_id')->unsigned();
+            $table->integer('year')->unsigned();
+            $table->string('name');
+            $table->string('distance');
+            $table->string('time');
+            $table->string('position');
+
+            $table->softDeletes();
+            $table->foreign('registration_id')->references('id')->on('registrations')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('registrant_past_results');
+    }
+}
